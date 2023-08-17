b20b1ee5ce5941005c8aa6268f9f30f4fb5e076f
Zayed <zayed.ahmed123321@gmail.com>
2023-08-17 16:22:42 +0400

    fixes to strlcpy

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -3,13 +3,19 @@
 size_t ft_strlcpy(char *dest, const char *src, size_t n)
 {
     size_t i;
+    size_t slen;
 
     i = 0;
-    while (i < n - 1 && src[i] != '\0' && n > 0)
+    slen = 0;
+    while (src[slen] != '\0')
+        slen++;
+    if (n == 0);
+        return (slen);
+    while (src[i] && i < n - 1)
     {
         dest[i] = src[i];
         i++;
     }
     dest[i] = '\0';
-    return (i);
+    return (slen);
 }
--- Libft/main.c
+++ Libft/main.c
@@ -2,22 +2,4 @@
 
 int main ()
 {
-    /* 1 */ printf("%d\n", ft_strncmp("t", "", 0) == 0); 
-	/* 2 */ printf("%d\n", ft_strncmp("1234", "1235", 3) == 0); 
-	/* 3 */ printf("%d\n", ft_strncmp("1234", "1235", 4) < 0); 
-	/* 4 */ printf("%d\n", ft_strncmp("1234", "1235", -1) < 0); 
-	/* 5 */ printf("%d\n", ft_strncmp("", "", 42) == 0); 
-	/* 6 */ printf("%d\n", ft_strncmp("Tripouille", "Tripouille", 42) == 0); 
-	/* 7 */ printf("%d\n", ft_strncmp("Tripouille", "tripouille", 42) < 0); 
-	/* 8 */ printf("%d\n", ft_strncmp("Tripouille", "TriPouille", 42) > 0); 
-	/* 9 */ printf("%d\n", ft_strncmp("Tripouille", "TripouillE", 42) > 0); 
-	/* 10 */ printf("%d\n", ft_strncmp("Tripouille", "TripouilleX", 42) < 0); 
-	/* 11 */ printf("%d\n", ft_strncmp("Tripouille", "Tripouill", 42) > 0); 
-	/* 12 */ printf("%d\n", ft_strncmp("", "1", 0) == 0); 
-	/* 13 */ printf("%d\n", ft_strncmp("1", "", 0) == 0); 
-	/* 14 */ printf("%d\n", ft_strncmp("", "1", 1) < 0); 
-	/* 15 */ printf("%d\n", ft_strncmp("1", "", 1) > 0); 
-	/* 16 */ printf("%d\n", ft_strncmp("", "", 1) == 0); 
-	write(1, "\n", 1);
-	return (0);
 }
