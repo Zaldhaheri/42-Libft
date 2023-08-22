a782c22d9425f1723fcd4e330f2211449af42c24
Zayed <zayed.ahmed123321@gmail.com>
2023-08-22 19:13:21 +0400

    added itoa to header and make

--- Libft/libft.h
+++ Libft/libft.h
@@ -16,6 +16,7 @@ int	ft_isalpha(int c);
 int ft_isascii(int c);
 int ft_isdigit(int c);
 int ft_isprint(int c);
+char *ft_itoa(int n);
 void *ft_memchr(const void *str, int c, size_t n);
 int ft_memcmp(const void *str1, const void *str2, size_t n);
 void *ft_memcpy(void *dest, const void *src, size_t n);
